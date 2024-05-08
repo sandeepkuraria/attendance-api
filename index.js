db61b43ae3eb6a05e71f2bca693941c4c8d35081
sandeepkuraria <sandeep.kuraria@gmail.com>
2024-05-08 07:14:57 +0530

    server.js renamed to index.js and .env and vercel.json file changes

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/auth", authRoutes);
-app.use("/", locationRoutes);
-
+// app.use("/", locationRoutes);
+app.get("/", (req, res) => {
+  res.send("products api running");
+});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
