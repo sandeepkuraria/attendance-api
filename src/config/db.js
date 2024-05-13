593042a49515dbe5e4ee9f1f7538ce2e901aeae4
sandeepkuraria <sandeep.kuraria@gmail.com>
2024-05-13 09:08:18 +0530

    port defined in env file

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
@@ -14,6 +15,14 @@ db.connect((err) => {
     return;
   }
   console.log("Connected to database");
+});
+// Create the database if it doesn't exist
+db.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_DATABASE}`, (err, result) => {
+  if (err) {
+    console.error("Error creating database: " + err.stack);
+    return;
+  }
+  console.log("Database created or already exists");
 });
 // Select the database
 db.query(`USE ${process.env.DB_DATABASE}`, (err, result) => {
